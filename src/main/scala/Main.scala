3e3eadfa9a4bd261daf38ba09f6abe7e66959a6e
Ryo Ota <nwtgck@gmail.com>
2017-05-29 11:04:42 +0900

    [refactor] Remove unnecessary code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,4 @@
-import com.sun.net.httpserver.Authenticator.Failure
-
-import scala.util.{Success, Try}
+import scala.util.Try
 
 sealed trait Cell
 case object Empty  extends Cell
@@ -122,8 +120,6 @@ object Minimax{
       if table((i, j)) == Empty
     } yield (i, j)
 
-    val evals = candidatePoss.map{pos => minimax(table, turn, turn, pos)}
-    if(false) println(s"Evals: ${evals}")
     candidatePoss.maxBy{pos =>
       minimax(table, turn, turn, pos)
     }
@@ -148,17 +144,13 @@ object Minimax{
 
 
     if(updatedTable.isFilled){
-      if(false) println("--------- filled -----------")
-      if(false) println(updatedTable)
-      val value = updatedTable.winnerOpt() match {
+
+      updatedTable.winnerOpt() match {
         // Win or Lose
         case Some(winner) => if(winner == fo) 1 else -1
         // Draw
         case None         => 0
       }
-      if(false) println(s"winnerOpt: ${updatedTable.winnerOpt()}, value: ${value}")
-      if(false) println("---------        -----------")
-      value
     } else {
       val evaluatedValues: Seq[Int] = for{
         i <- 0 to 2
@@ -169,29 +161,12 @@ object Minimax{
         minimax(updatedTable, fo, nextTurn, (i, j))
       }
 
-      if(false) println(s"evaluated values: ${evaluatedValues}")
-
       if(fo == turn){
         evaluatedValues.min
       } else {
         evaluatedValues.max
       }
     }
-
-//    ???
-
-//    // Game finished
-//    if(table.isFilled){
-//      table.winnerOpt() match {
-//        // Win or Lose
-//        case Some(winner) => if(winner == fo) 1 else -1
-//        // Draw
-//        case None         => 0
-//      }
-//    } else {
-//
-//    }
-
   }
 }
 
@@ -201,129 +176,58 @@ object Minimax{
 object Main {
   def main(args: Array[String]): Unit = {
 
-    if(false){
-      println(Table.initial())
-      //    - - -
-      //    - - -
-      //    - - -
-
-      println(Table.initial().updated((0, 0), Circle)) // Top left
-      //    O - -
-      //    - - -
-      //    - - -
-
-      println(Table.initial().updated((2, 2), Circle)) // Bottom right
-      println(Table.initial().updated((1, 1), Circle)) // Middle center
-      println(Table.initial().updated((1, 0), Circle)) //
-    }
-
-
-    if(false){
-      var t = Table.initial()
-      t = t.updated((0, 0), Circle)
-      t = t.updated((1, 0), Circle)
-      t = t.updated((2, 0), Circle)
-      println(t.winnerOpt())
-    }
-
-    if(false){
-      var t = Table.initial()
-      t = t.updated((0, 0), Cross)
-      t = t.updated((0, 1), Circle)
-      t = t.updated((0, 2), Circle)
-      t = t.updated((1, 0), Circle)
-      t = t.updated((1, 1), Cross)
-      t = t.updated((2, 0), Cross)
-      println(t)
-      println(t.winnerOpt())
-
-      println(s"Filled: ${t.isFilled}")
-
-      for{
-        i <- 0 to 2
-        j <- 0 to 2
-      } {
-        if(t((i,j)) == Empty){
-          println(s"EMPTY ${(i, j)}")
-//          t = t.updated((i, j), Circle)
-        }
-      }
-
-      println(s"Filled: ${t.isFilled}")
-    }
-
-
-
-    if(false){
-      var t = Table.initial()
-      t = t.updated((0, 0), Cross)
-      t = t.updated((0, 1), Circle)
-      t = t.updated((0, 2), Circle)
-      t = t.updated((1, 0), Circle)
-      t = t.updated((1, 1), Cross)
-      t = t.updated((2, 0), Cross)
-      println(t)
-
-      println(Minimax.bestPos(t, Circle))
-
-      val bestPos = Minimax.bestPos(t, Circle)
-      println(bestPos)
-      println(t)
-      println(t.updated(bestPos, Circle))
-    }
-
-    def playerPos(table: Table): (Int, Int) = {
-      var i: Int = 0
-      var j: Int = 0
-
-      var triedPos: Try[(Int, Int)] = null
-
-      do{
-        val triedInt: Try[Int] = Try(scala.io.StdIn.readLine().toInt)
-        triedPos = Try{
-          val ij: Int = triedInt.get
-          val i: Int = ij / 10
-          val j: Int = ij % 10
-          require(0 <= i && i <= 2)
-          require(0 <= j && j <= 2)
-          require(table((i, j)) == Empty)
-          (i, j)
-        }
-      } while (triedPos.isFailure)
-
-
-      triedPos.get
-    }
-
-
-    if(true){
-      var table = Table.initial()
-      var turn: Cell  = Circle /** Restrict: cell != turn **/
-      val human: Cell = Circle /** Restrict: cell != turn **/
-      while(!table.isFilled && table.winnerOpt().isEmpty) {
-        println(table)
-
-        val pos: (Int, Int) = if(turn == human){
-          playerPos(table)
-        } else {
-          Minimax.bestPos(table, turn)
-        }
-
-        table = table.updated(pos, turn)
-        turn = Minimax.flipCell(turn)
-      }
+    var table = Table.initial()
+    var turn: Cell = Circle
+    /** Restrict: cell != turn **/
+    val human: Cell = Circle /** Restrict: cell != turn **/
+    while (!table.isFilled && table.winnerOpt().isEmpty) {
       println(table)
-      table.winnerOpt() match {
-        case Some(winner) =>
-          println(s"Winner: ${winner}")
-        case None =>
-          println("Result: draw")
+
+      val pos: (Int, Int) = if (turn == human) {
+        playerPos(table)
+      } else {
+        Minimax.bestPos(table, turn)
       }
+
+      table = table.updated(pos, turn)
+      turn = Minimax.flipCell(turn)
+    }
+    println(table)
+    table.winnerOpt() match {
+      case Some(winner) =>
+        println(s"Winner: ${winner}")
+      case None =>
+        println("Result: draw")
     }
 
+  }
 
 
+  /**
+    * Get player position
+    * @param table
+    * @return
+    */
+  def playerPos(table: Table): (Int, Int) = {
+    var i: Int = 0
+    var j: Int = 0
+
+    var triedPos: Try[(Int, Int)] = null
+
+    do{
+      val triedInt: Try[Int] = Try(scala.io.StdIn.readLine().toInt)
+      triedPos = Try{
+        val ij: Int = triedInt.get
+        val i: Int = ij / 10
+        val j: Int = ij % 10
+        require(0 <= i && i <= 2)
+        require(0 <= j && j <= 2)
+        require(table((i, j)) == Empty)
+        (i, j)
+      }
+    } while (triedPos.isFailure)
 
 
+    triedPos.get
   }
 }
